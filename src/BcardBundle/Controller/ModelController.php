d645795e878eda2f2af98e8d0f544ea6b285c8c2
saliih <salah.chtioui@gmail.com>
2017-05-19 23:07:08 +0100

    svg

--- src/BcardBundle/Controller/ModelController.php
+++ src/BcardBundle/Controller/ModelController.php
@@ -3,11 +3,132 @@
 namespace BcardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ModelController extends Controller
 {
     public function indexAction()
     {
         return $this->render('BcardBundle:Model:index.html.twig', array());
+    }
+
+    public function generateFormAction(Request $request)
+    {
+        $type = $request->request->get('type');
+        $data['id'] = $request->request->get('id');
+        switch ($type) {
+            case "rect":
+                $extra = array(
+                    "stroke" => $request->request->get('stroke'),
+                    "fill" => $request->request->get('fill'),
+                    //"x" =>$request->request->get('x'),
+                    // "y" =>$request->request->get('y'),
+                    //"width" =>$request->request->get('width'),
+                    //"height" =>$request->request->get('height'),
+                );
+                break;
+            case "image":
+                $extra = array("file" => "file");
+                break;
+            case "text":
+                $extra = array(
+                    "text" => $request->request->get('text'),
+                    "stroke" => $request->request->get('stroke'),
+                    "fill" => $request->request->get('fill'),
+                    "fontfamily" => $request->request->get('fontfamily'),
+                    "fontsize" => $request->request->get('fontsize'),
+                );
+                break;
+            case "circle":
+                $extra = array(
+                    "stroke" => $request->request->get('stroke'),
+                    "fill" => $request->request->get('fill'),
+                    "strokewidth" => $request->request->get('strokewidth'),
+                );
+                break;
+            case "path":
+                $extra = array(
+                    "stroke" => $request->request->get('stroke'),
+                    "fill" => $request->request->get('fill'),
+                    "strokewidth" => $request->request->get('strokewidth'),
+                );
+                break;
+        }
+        $html = "";
+        $data = array_merge($data, $extra);
+        foreach ($data as $key => $value) {
+            if ($key == 'id') continue;
+            else if ($key == "stroke") {
+                $html .= $this->renderView("BcardBundle:Fields:color.html.twig",
+                    array(
+                        'title' => "Couleur de trait",
+                        'value' => $value,
+                        'key' => $key,
+                        'id' => $data['id']
+                    ));
+            }
+            else if ($key == "fill") {
+                $html .= $this->renderView("BcardBundle:Fields:color.html.twig",
+                    array(
+                        'title' => "Couleur ",
+                        'value' => $value,
+                        'key' => $key,
+                        'id' => $data['id']
+                    ));
+            }
+            else if ($key == "fontfamily") {
+                $html .= $this->renderView("BcardBundle:Fields:fontfamily.html.twig",
+                    array(
+                        'title' => "Police ",
+                        'value' => $value,
+                        'key' => $key,
+                        'id' => $data['id']
+                    ));
+            }
+            else if ($key == "fontsize") {
+                $html .= $this->renderView("BcardBundle:Fields:fontsize.html.twig",
+                    array(
+                        'title' => "Taille de police ",
+                        'value' => $value,
+                        'key' => $key,
+                        'id' => $data['id']
+                    ));
+            }
+            else if ($key == "text") {
+                $html .= $this->renderView("BcardBundle:Fields:text.html.twig",
+                    array(
+                        'title' => "Text ",
+                        'value' => $value,
+                        'key' => $key,
+                        'id' => $data['id']
+                    ));
+            }
+            else if ($key == "file") {
+                $html .= $this->renderView("BcardBundle:Fields:file.html.twig",
+                    array(
+                        'title' => "Logo",
+                        'key' => $key,
+                        'id' => $data['id']
+                    ));
+            }
+        }
+        return new Response($html);
+    }
+
+    public function uploadfileAction(Request $request){
+        $file = $request->files->get('filelogo');
+        $time = time()+rand(1,61561). $file->getClientOriginalExtension();
+        $file->move(
+            $this->getParameter('kernel.root_dir') . "/../web/uploads/",
+            $time
+        );
+        $filename =  $this->getParameter('kernel.root_dir') . "/../web/uploads/".$time;
+        $type = pathinfo($filename, PATHINFO_EXTENSION);
+        $data = file_get_contents($filename);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        return new Response($base64);
+
     }
 }
--- src/BcardBundle/Resources/public/js/bcard.js
+++ src/BcardBundle/Resources/public/js/bcard.js
@@ -2,54 +2,151 @@
  * Created by sarra on 16/05/17.
  */
 $(document).ready(function () {
-    $('.bcard svg g').children().on('click',function (event) {
-        var obj = {}
-        if($(this).is('rect')){
+    $('.bcard svg g').children().on('click', function (event) {
+        $('.formedition').css('display', 'block');
+        var obj = {};
+        if ($(this).is('rect')) {
             obj = {
                 type: 'rect',
-                id:$(this).attr('id'),
-                stroke:$(this).attr('stroke'),
-                fill:$(this).attr('fill'),
-                x:$(this).attr('x'),
-                y:$(this).attr('y'),
-                width:$(this).attr('width'),
-                height:$(this).attr('height'),
+                id: $(this).attr('id'),
+                stroke: $(this).attr('stroke'),
+                fill: $(this).attr('fill'),
+                x: $(this).attr('x'),
+                y: $(this).attr('y'),
+                width: $(this).attr('width'),
+                height: $(this).attr('height'),
             };
-        }else if($(this).is('text')){
-            obj={
+        } else if ($(this).is('image')) {
+            obj = {
+                type: 'image',
+                id: $(this).attr('id'),
+            }
+        } else if ($(this).is('text')) {
+            obj = {
                 type: 'text',
-                id:$(this).attr('id'),
-                stroke:$(this).attr('stroke'),
-                fill:$(this).attr('fill'),
-                x:$(this).attr('x'),
-                y:$(this).attr('y'),
-                width:$(this).attr('width'),
-                height:$(this).attr('height'),
-                fontfamily:$(this).attr('font-family'),
-                fontsize:$(this).attr('font-size'),
+                id: $(this).attr('id'),
+                text: this.textContent,
+                stroke: $(this).attr('stroke'),
+                fill: $(this).attr('fill'),
+                x: $(this).attr('x'),
+                y: $(this).attr('y'),
+                width: $(this).attr('width'),
+                height: $(this).attr('height'),
+                fontfamily: $(this).attr('font-family'),
+                fontsize: $(this).attr('font-size'),
             }
-        }else if($(this).is('circle')){
-            obj={
+        } else if ($(this).is('circle')) {
+            obj = {
                 type: 'circle',
-                id:$(this).attr('id'),
-                stroke:$(this).attr('stroke'),
-                strokewidth:$(this).attr('stroke-width'),
-                fill:$(this).attr('fill'),
-                cx:$(this).attr('cx'),
-                cy:$(this).attr('cy'),
-                r:$(this).attr('r'),
+                id: $(this).attr('id'),
+                stroke: $(this).attr('stroke'),
+                strokewidth: $(this).attr('stroke-width'),
+                fill: $(this).attr('fill'),
+                cx: $(this).attr('cx'),
+                cy: $(this).attr('cy'),
+                r: $(this).attr('r'),
             }
-        }else if($(this).is('path')){
-            obj={
+        } else if ($(this).is('path')) {
+            obj = {
                 type: 'path',
-                id:$(this).attr('id'),
-                fill:$(this).attr('fill'),
-                stroke:$(this).attr('stroke'),
-                strokewidth:$(this).attr('stroke-width'),
-                fill:$(this).attr('fill'),
+                id: $(this).attr('id'),
+                fill: $(this).attr('fill'),
+                stroke: $(this).attr('stroke'),
+                strokewidth: $(this).attr('stroke-width'),
+                fill: $(this).attr('fill'),
             }
         }
-console.log(obj)
         window.element = obj;
+
+        getRequest(Routing.generate('bcard_generate_form'), obj, function (html) {
+            $('.formedition').html('').html(html);
+            $('.colorpicker').colorpicker({
+                customClass: 'colorpicker-2x',
+                format: 'hex',
+                sliders: {
+                    saturation: {
+                        maxLeft: 200,
+                        maxTop: 200
+                    },
+                    hue: {
+                        maxTop: 200
+                    },
+                    alpha: {
+                        maxTop: 200
+                    }
+                }
+            }).on('changeColor', function (event) {
+                changeBlock(event.currentTarget);
+            });
+            $('.formedition input, .formedition select')
+                .not('.colorpicker')
+                .not('input[type=file]')
+                .on('change', function (event) {
+                changeBlock(event.currentTarget);
+            });
+            $('.formedition input[type=file]').on('change',function (event) {
+               $(event.currentTarget).closest('form').submit();
+            });
+            $('#formlogo').on('submit',function (event) {
+                var data = new FormData(this);
+                var url = $(this).attr('action');
+
+                getRequest(url, data, function (result) {
+                    var id = $("input[type=file]").attr("data-id");
+                    $('#'+id).attr('xlink:href',result);
+                }, {type:"POST"}, {
+                    cache: false,
+                    dataType: 'html',
+                    mimeType: "multipart/form-data",
+                    contentType: false,
+                    processData: false
+                });
+                return false;
+            });
+        })
     })
 });
+function changeBlock(input) {
+    var val = $(input).val();
+    var id = $(input).attr("data-id");
+    var attr = $(input).attr('data-element');
+    if (attr == "fontfamily")attr = "font-family";
+    if (attr == "fontsize")attr = "font-size";
+    if (attr == "text") {
+        $("#" + id).text(val);
+    } else
+        $("#" + id).attr(attr, val);
+}
+
+function getRequest(url, _data, success_function, params, extraParams) {
+    params = params || {};
+    var showloader = params.showloader || true;
+
+    var parameters = {
+        async: true,
+        url: url,
+        type: params.type || "POST",
+        data: _data,
+        success: function (data) {
+            if (data === "is_not_logged") {
+                successDialog("Nyellow", "Session expired! Please login again.", BootstrapDialog.TYPE_WARNING, undefined, function () {
+                    window.location.href = "login";
+                });
+            } else {
+                if (typeof (success_function) == "function") {
+                    success_function(data);
+                }
+            }
+        },
+        error: function (xhr) {
+            errorDialog('Error contacting server', 'Error encountered : ' + xhr.responseText);
+        },
+        complete: function () {
+
+        }
+    };
+    if (typeof(extraParams) == "object") {
+        $.extend(parameters, extraParams);
+    }
+    $.ajax(parameters);
+}
