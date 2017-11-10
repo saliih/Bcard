1d956d69eb06ece36bbaa1fb4fdba4d0be6d55cc
Salah Chtioui <salah.chtioui@gmail.com>
2017-11-10 19:18:43 +0100

    exec

--- src/BcardBundle/Controller/AdminController.php
+++ src/BcardBundle/Controller/AdminController.php
@@ -31,78 +31,84 @@ class AdminController extends Controller
     }
     public function generatePdfAction($id)
     {
-        $invoice = $this->getDoctrine()->getRepository('BcardBundle:Invoice')->find($id);
-         $recto = $invoice->getRecto();
-        $verso = $invoice->getVerso();
-        $path = $this->get('kernel')->getRootDir() . '/../web/uploads/';
-        $html =   htmlspecialchars_decode(file_get_contents($path.$recto));
-        $html = preg_replace('/(<p.+?)unicode=".+?"(>.+?)/i', "$1$2", $html);
-        $html = str_replace('unicode="<"', "", $html);
-        $html = str_replace('unicode=">"', "", $html);
-        $html = str_replace('unicode="&"', "", $html);
-        $html = str_replace('unicode="""', "", $html);
-        $html = str_replace('unicode="&nbsp;"', "", $html);
-        //$html = preg_replace('/(<[^>]+) unicode=".*?"/i', '$1', $html);
-        /*$html = preg_replace("/<\/?div[^>]*\>/i", "", $html);
-        //echo $html;exit;
-        file_put_contents($path."toexport/".$id.".svg",$html);
-echo "inkscape ".$path.$recto." --export-pdf=".$path."toexport/".$id.".pdf";exit;
-        exec("inkscape ".$path.$recto." --export-pdf=".$path."toexport/".$id.".pdf", $output, $return_var);
-//echo "<pre>";print_r($output);print_r($return_var);exit;
-        return new BinaryFileResponse($path."toexport/".$id.".pdf");
+        try{
+            $invoice = $this->getDoctrine()->getRepository('BcardBundle:Invoice')->find($id);
+            $recto = $invoice->getRecto();
+            $verso = $invoice->getVerso();
+            $path = $this->get('kernel')->getRootDir() . '/../web/uploads/';
+            $html =   htmlspecialchars_decode(file_get_contents($path.$recto));
+            $html = preg_replace('/(<p.+?)unicode=".+?"(>.+?)/i', "$1$2", $html);
+            $html = str_replace('unicode="<"', "", $html);
+            $html = str_replace('unicode=">"', "", $html);
+            $html = str_replace('unicode="&"', "", $html);
+            $html = str_replace('unicode="""', "", $html);
+            $html = str_replace('unicode="&nbsp;"', "", $html);
+            //$html = preg_replace('/(<[^>]+) unicode=".*?"/i', '$1', $html);
+            /*$html = preg_replace("/<\/?div[^>]*\>/i", "", $html);
+            //echo $html;exit;
+            file_put_contents($path."toexport/".$id.".svg",$html);
+    echo "inkscape ".$path.$recto." --export-pdf=".$path."toexport/".$id.".pdf";exit;
+            exec("inkscape ".$path.$recto." --export-pdf=".$path."toexport/".$id.".pdf", $output, $return_var);
+    //echo "<pre>";print_r($output);print_r($return_var);exit;
+            return new BinaryFileResponse($path."toexport/".$id.".pdf");
 
 
-        exit;*/
-        preg_match( '/width="([^"]*)"/i', $html, $arraywidth ) ;
-        preg_match( '/height="([^"]*)"/i', $html, $arrayheight ) ;
-        $unit = "mm";
-        $height = 61;
-        $width = 91;
-        if(isset($arraywidth[1])){
-            $width =(float) $arraywidth[1];
-            $unit = "pt";
-        }
-        if(isset($arrayheight[1])){
-            $height =(float) $arrayheight[1];
-            $unit = "pt";
-        }
-        /*echo "<pre>";
-        print_r(array($height,$width,$unit));
-        exit;
-*/
-        $pdf = $this->get("white_october.tcpdf")
-            ->create('vertical', $unit, array($width, $height), true, 'UTF-8', false);
-
-        $fontname = $pdf->addTTFfont($this->get('kernel')->getRootDir() . '/../web/FreeSerifItalic.ttf', 'TrueTypeUnicode', '', 96);
-
-// use the font
-        $pdf->SetFont($fontname, '', 14, '', false);
-        $pdf->SetMargins(40, 0, 40, false);
-        $pdf->SetAutoPageBreak(true, 0);
-        $pdf->setFontSubsetting(false);
-        $pdf->AddPage();
-
-        $pdf->ImageSVG($path .$recto, 3, 3, $width, $height);
-     //   $this->image($pdf,$path .$recto);
-        if($verso!="") {
-            preg_match( '/width="([^"]*)"/i', file_get_contents($path.$verso), $arraywidth ) ;
-            preg_match( '/width="([^"]*)"/i', file_get_contents($path.$verso), $arrayheight ) ;
-            $height = 55;
-            $width = 85;
+            exit;*/
+            preg_match( '/width="([^"]*)"/i', $html, $arraywidth ) ;
+            preg_match( '/height="([^"]*)"/i', $html, $arrayheight ) ;
+            $unit = "mm";
+            $height = 61;
+            $width = 91;
             if(isset($arraywidth[1])){
-                $width = (float) $arraywidth[1];
+                $width =(float) $arraywidth[1];
+                $unit = "pt";
             }
             if(isset($arrayheight[1])){
-                $height = (float) $arrayheight[1];
+                $height =(float) $arrayheight[1];
+                $unit = "pt";
             }
+            /*echo "<pre>";
+            print_r(array($height,$width,$unit));
+            exit;
+    */
+            $pdf = $this->get("white_october.tcpdf")
+                ->create('vertical', $unit, array($width, $height), true, 'UTF-8', false);
 
+            $fontname = $pdf->addTTFfont($this->get('kernel')->getRootDir() . '/../web/FreeSerifItalic.ttf', 'TrueTypeUnicode', '', 96);
+
+// use the font
+            $pdf->SetFont($fontname, '', 14, '', false);
+            $pdf->SetMargins(40, 0, 40, false);
+            $pdf->SetAutoPageBreak(true, 0);
+            $pdf->setFontSubsetting(false);
             $pdf->AddPage();
 
-            $pdf->ImageSVG($path . $verso, 0, 0, $width, $height, '','', '', 0, true);
-            $this->image($pdf,$path .$verso);
+            $pdf->ImageSVG($path .$recto, 3, 3, $width, $height);
+            //   $this->image($pdf,$path .$recto);
+            if($verso!="") {
+                preg_match( '/width="([^"]*)"/i', file_get_contents($path.$verso), $arraywidth ) ;
+                preg_match( '/width="([^"]*)"/i', file_get_contents($path.$verso), $arrayheight ) ;
+                $height = 55;
+                $width = 85;
+                if(isset($arraywidth[1])){
+                    $width = (float) $arraywidth[1];
+                }
+                if(isset($arrayheight[1])){
+                    $height = (float) $arrayheight[1];
+                }
+
+                $pdf->AddPage();
+
+                $pdf->ImageSVG($path . $verso, 0, 0, $width, $height, '','', '', 0, true);
+                $this->image($pdf,$path .$verso);
+            }
+            $filename = 'caret_visite';
+            $pdf->Output($filename . ".pdf", 'I'); // This will output the PDF as a response directly
+
         }
-        $filename = 'caret_visite';
-        $pdf->Output($filename . ".pdf", 'I'); // This will output the PDF as a response directly
+        catch (\Exception $e){
+            return new Response($e->getMessage());
+        }
 
     }
     private function image($pdf,$svg){
