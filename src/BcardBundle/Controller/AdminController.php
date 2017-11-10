8fbc25edebb7ab6fd5d527dfaa72c4b3727c0cc5
Salah Chtioui <salah.chtioui@gmail.com>
2017-11-10 19:23:38 +0100

    exec

--- src/BcardBundle/Controller/AdminController.php
+++ src/BcardBundle/Controller/AdminController.php
@@ -44,16 +44,16 @@ class AdminController extends Controller
             $html = str_replace('unicode="""', "", $html);
             $html = str_replace('unicode="&nbsp;"', "", $html);
             //$html = preg_replace('/(<[^>]+) unicode=".*?"/i', '$1', $html);
-            /*$html = preg_replace("/<\/?div[^>]*\>/i", "", $html);
+            $html = preg_replace("/<\/?div[^>]*\>/i", "", $html);
             //echo $html;exit;
             file_put_contents($path."toexport/".$id.".svg",$html);
-    echo "inkscape ".$path.$recto." --export-pdf=".$path."toexport/".$id.".pdf";exit;
+    //echo "inkscape ".$path.$recto." --export-pdf=".$path."toexport/".$id.".pdf";exit;
             exec("inkscape ".$path.$recto." --export-pdf=".$path."toexport/".$id.".pdf", $output, $return_var);
     //echo "<pre>";print_r($output);print_r($return_var);exit;
             return new BinaryFileResponse($path."toexport/".$id.".pdf");
 
 
-            exit;*/
+            exit;
             preg_match( '/width="([^"]*)"/i', $html, $arraywidth ) ;
             preg_match( '/height="([^"]*)"/i', $html, $arrayheight ) ;
             $unit = "mm";
@@ -74,10 +74,6 @@ class AdminController extends Controller
             $pdf = $this->get("white_october.tcpdf")
                 ->create('vertical', $unit, array($width, $height), true, 'UTF-8', false);
 
-            $fontname = $pdf->addTTFfont($this->get('kernel')->getRootDir() . '/../web/FreeSerifItalic.ttf', 'TrueTypeUnicode', '', 96);
-
-// use the font
-            $pdf->SetFont($fontname, '', 14, '', false);
             $pdf->SetMargins(40, 0, 40, false);
             $pdf->SetAutoPageBreak(true, 0);
             $pdf->setFontSubsetting(false);
